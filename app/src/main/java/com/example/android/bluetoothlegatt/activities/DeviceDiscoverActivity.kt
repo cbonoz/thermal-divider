a565dc52299a1e9976eab55ae40e80d0e902dfb2
cbonoz <chrisdistrict@gmail.com>
2018-08-14 21:01:36 +0800

    add scanning gif

--- app/src/main/java/com/example/android/bluetoothlegatt/activities/DeviceDiscoverActivity.kt
+++ app/src/main/java/com/example/android/bluetoothlegatt/activities/DeviceDiscoverActivity.kt
@@ -14,6 +14,8 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
 import com.example.android.bluetoothlegatt.R
 import timber.log.Timber
@@ -24,9 +26,11 @@ import timber.log.Timber
  */
 class DeviceDiscoverActivity : Activity(), BluetoothAdapter.LeScanCallback {
 
-    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private lateinit var mBluetoothAdapter: BluetoothAdapter
     private var mScanning: Boolean = false
     private var mHandler: Handler? = null
+
+    private var loadingLayout: LinearLayout? = null
 
     override fun onLeScan(device: BluetoothDevice?, rssi: Int, scanRecord: ByteArray?) {
         if (device != null) {
@@ -36,8 +40,7 @@ class DeviceDiscoverActivity : Activity(), BluetoothAdapter.LeScanCallback {
                 intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.name)
                 intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.address)
                 if (mScanning) {
-                    mBluetoothAdapter!!.stopLeScan(this)
-                    mScanning = false
+                    stopBluetoothScan()
                 }
                 Toast.makeText(this, "Connected: Started Monitoring", Toast.LENGTH_SHORT).show()
                 startActivity(intent)
@@ -53,6 +56,9 @@ class DeviceDiscoverActivity : Activity(), BluetoothAdapter.LeScanCallback {
             actionBar!!.setTitle(R.string.title_devices)
         }
         setContentView(R.layout.activity_device_discover)
+
+        loadingLayout = findViewById(R.id.loadingLayout)
+
         mHandler = Handler()
 
         // Use this check to determine whether BLE is supported on the device.  Then you can
@@ -62,20 +68,15 @@ class DeviceDiscoverActivity : Activity(), BluetoothAdapter.LeScanCallback {
             return
         }
 
-        // Prompt for permissions
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_RESPONSE)
-        }
-
         // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
         // BluetoothAdapter through BluetoothManager.
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         mBluetoothAdapter = bluetoothManager.adapter
 
-        // Checks if Bluetooth is supported on the device.
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show()
-            finish()
+        // Prompt for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_RESPONSE)
         }
     }
 
@@ -107,20 +108,17 @@ class DeviceDiscoverActivity : Activity(), BluetoothAdapter.LeScanCallback {
 
         // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
         // fire an intent to display a dialog asking the user to grant permission to enable it.
-        if (mBluetoothAdapter != null) {
-            if (!mBluetoothAdapter!!.isEnabled) {
-                if (!mBluetoothAdapter!!.isEnabled) {
-                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                }
-            }
-            scanLeDevice(true)
+        if (!mBluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
+        scanLeDevice(true)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         // User chose not to enable Bluetooth.
-        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+        if (data == null || (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED)) {
+            Toast.makeText(this, getString(R.string.bluetooth_required), Toast.LENGTH_SHORT).show()
             finish()
             return
         }
@@ -133,21 +131,35 @@ class DeviceDiscoverActivity : Activity(), BluetoothAdapter.LeScanCallback {
     }
 
     private fun scanLeDevice(enable: Boolean) {
+
         if (enable) {
             // Stops scanning after a pre-defined scan period.
             mHandler!!.postDelayed({
-                mScanning = false
-                mBluetoothAdapter!!.stopLeScan(this)
+                stopBluetoothScan()
                 invalidateOptionsMenu()
             }, SCAN_PERIOD)
 
-            mScanning = true
-            mBluetoothAdapter!!.startLeScan(this)
+            startBluetoothScan()
         } else {
-            mScanning = false
-            mBluetoothAdapter!!.stopLeScan(this)
+            stopBluetoothScan()
         }
         invalidateOptionsMenu()
+    }
+
+    fun startBluetoothScan() {
+        loadingLayout?.visibility = View.VISIBLE
+        mScanning = true
+        mBluetoothAdapter.startLeScan(this)
+    }
+
+    fun stopBluetoothScan() {
+        loadingLayout?.visibility = View.INVISIBLE
+        mScanning = false
+        mBluetoothAdapter.stopLeScan(this)
+        Toast.makeText(this,
+                "Could not find BLE Device $THERMAL_DIVIDER_DEVICE_NAME. Restart scan via the options menu to search again",
+                Toast.LENGTH_SHORT)
+                .show()
     }
 
     companion object {
